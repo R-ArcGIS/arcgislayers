ab1b5e613c1e29acfe81435bc3a3117a791c3d89
Josiah Parry <josiah.parry@gmail.com>
2023-03-02 13:52:20 -0500

    add password legacy auth

--- R/auth.R
+++ R/auth.R
@@ -112,6 +112,53 @@ auth_code <- function(client = Sys.getenv("ARCGIS_CLIENT"),
 
 }
 
+
+# legacy auth -------------------------------------------------------------
+
+
+# uses request ip does not support other forms
+# use oauth as the recommended approach
+#' @export
+#' @rdname auth
+auth_password <- function(
+    username = Sys.getenv("ARCGIS_USER"),
+    password = Sys.getenv("ARCGIS_PASSWORD"),
+    host = "https://www.arcgis.com",
+    expiration = 60
+) {
+
+  if (expiration > 21600) stop("`expiration` cannot be more than 15 days (21600)")
+
+  burl <- paste0(host, "/sharing/rest/generateToken")
+  req <- httr2::request(burl) |>
+    httr2::req_body_form(
+      username = username,
+      password = password,
+      client = "requestip",
+      expiration = expiration,
+      f = "json"
+    )
+
+  resp <- httr2::req_perform(req)
+
+  token <- httr2::resp_body_json(resp)
+
+  # handle errors
+  if (is.null(token[["token"]])) {
+    stop(
+      "\n  Status code: ", token[["error"]][["code"]], "\n  ", token$error$message,
+      call. = FALSE
+    )
+  }
+
+  # return the token
+  httr2::oauth_token(
+    token[["token"]],
+    expires_in = expiration
+  )
+
+}
+
 #' @export
 #' @rdname auth
 set_auth_token <- function(token, quietly = FALSE) {
@@ -129,6 +176,7 @@ set_auth_token <- function(token, quietly = FALSE) {
   cat("Token set to environment variable `ARCGIS_TOKEN`")
   invisible(token)
 }
+
 
 
 
