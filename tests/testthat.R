edb51cfdf68ba2417819e09a8a881d46da12ff35
Josiah Parry <josiah.parry@gmail.com>
2023-07-03 09:35:28 -0400

    add publishing tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(arcgis)
+library(arcgislayers)
 
-test_check("arcgis")
+test_check("arcgislayers")
---
+++ tests/testthat/test-write-feature-layer.R
@@ -0,0 +1,22 @@
+test_that("Feature Layer can be written from an sf object ", {
+  skip("Must be ran interactively")
+  skip_if_not_installed("sf")
+
+  nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"))
+  tkn <- auth_code()
+  set_auth_token(tkn)
+  res <- publish_layer(nc, "NC test")
+
+  expect_null(res$services$success)
+})
+
+
+test_that("Table can be written from an sf object ", {
+  skip("Must be ran interactively")
+
+
+  tkn <- auth_code()
+  set_auth_token(tkn)
+  res <- publish_layer(iris, "Iris Test")
+  expect_null(res$services$success)
+})
