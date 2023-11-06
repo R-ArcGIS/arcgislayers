d1e8c52bd131fa2948c174be9eb3a63b206b3520
Josiah Parry <josiah.parry@gmail.com>
2023-11-06 08:01:03 -0500

    closes #38

---
+++ tests/testthat/test-date-parsing.R
@@ -0,0 +1,5 @@
+test_that("date parsing works", {
+  furl <- "https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/MTBS_Polygons_v1/FeatureServer/0"
+  res <- arc_select(arc_open(furl), n = 5, fields = "StartDate")
+  expect_s3_class(res$StartDate, c("POSIXct", "POSIXt"))
+})
