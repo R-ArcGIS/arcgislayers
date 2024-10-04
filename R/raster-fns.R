3ea96425e779cc76a6b72c9d4f1699a73c134170
Josiah Parry <josiah.parry@gmail.com>
2024-10-03 17:14:31 -0700

    document list service rastyer fns

--- R/raster-fns.R
+++ R/raster-fns.R
@@ -6,9 +6,17 @@
 #' @inheritParams arcgisutils::infer_esri_type
 #' @param x an `ImageServer`.
 #' @returns a data.frame of the available raster functions.
+#' @export
 #' @examples
-#' # example code
-#'
+#' # use paste to avoid cran note
+#' furl <- paste0(
+#'   "https://di-usfsdata.img.arcgis.com/arcgis/rest/services",
+#'   "/FIA_BIGMAP_2018_Species_Aboveground_Biomass/ImageServer"
+#' )
+#' 
+#' service <- arc_open(furl)
+#' raster_fns <- list_service_raster_fns(service)
+#' head(raster_fns)
 list_service_raster_fns <- function(x, arg = rlang::caller_arg(x), call = rlang::caller_call()) {
   check_inherits_any(x, "ImageServer")
 
