a7c0f4508ea882f8faea6060bbc84b84e3e36135
Eli Pousson <eli.pousson@gmail.com>
2025-05-19 21:51:50 -0400

    Finish working `update_definition()` function
    Also remove incomplete .update_definition_name and .update_layer_properties helper functions

--- R/update_definition.R
+++ R/update_definition.R
@@ -1,117 +1,92 @@
-#' Update Feature Layer or Service definition
+#' Update a Feature Layer or Feature Service definition
 #'
-#' <https://developers.arcgis.com/rest/services-reference/online/update-definition-feature-layer-.htm#ESRI_SECTION2_027B8A04953C409E9A9D9051DD4CDBEE>
-#' <https://developers.arcgis.com/rest/services-reference/online/update-definition-feature-service-.htm>
+#' [update_definition()] supports updating a definition property for a hosted
+#' Feature Service or Feature Layer. Examples of properties include the layer
+#' name, renderer, or field properties. Parameters passed to `...` must have
+#' names matching the definitions. Parameters are converted to a JSON
+#' `updateDefinition` parameter using [jsonify::to_json()]. See the ArcGIS REST API documentation on Administer Hosted Feature Services
+#' for more details on updating the definition for [a
+#' FeatureService](https://developers.arcgis.com/rest/services-reference/online/update-definition-feature-service-.htm)
+#' or [a
+#' FeatureLayer](https://developers.arcgis.com/rest/services-reference/online/update-definition-feature-layer/).
 #'
 #' @param x A Feature Layer or Feature Service class object.
-#' @param update A named list used as the "updateDefinition" body for the
-#'   request.
+#' @param ... Additional parameters "updateDefinition" body for the request.
 #' @param async Default `FALSE`. If `TRUE`, support asynchronous processing for
 #'   the request.
+#' @inheritParams arc_open
 #' @export
 update_definition <- function(
-    x,
-    update = NULL,
-    async = FALSE,
-    token = arc_token()) {
+  x = NULL,
+  ...,
+  async = FALSE,
+  token = arc_token()
+) {
   check_inherits_any(x, c("FeatureServer", "FeatureLayer"))
 
   req <- arc_base_req(
-    url = x[["url"]],
+    # Modify URL to be `adminservicecatalog-url`
+    # See <https://developers.arcgis.com/rest/services-reference/online/>
+    url = sub(
+      "rest/services",
+      "rest/admin/services",
+      x[["url"]]
+    ),
     token = token,
     path = "updateDefinition"
   )
 
-  req <- httr2::req_body_json(
-    req,
-    list(
-      updateDefinition = update,
-      async = tolower(async),
-      f = "json"
+  # TODO: Add check for existence of definition properties
+  # See https://developers.arcgis.com/rest/services-reference/online/add-to-definition-feature-layer/
+  update_definition <- rlang::list2(...)
+
+  # Customize theme for messages
+  dl_theme <- cli::cli_div(
+    theme = list(
+      span.dt = list(before = "`", after = "`"),
+      span.dd = list(color = "blue")
     )
+  )
+
+  # Pull existing service/layer definition values
+  definition <- x[names(x) %in% names(update_definition)]
+
+  # Display existing service/layer definition values
+  if (length(definition) > 0 && any(definition != "")) {
+    cli::cli_bullets(c("i" = "Existing definition values:"))
+    cli::cli_dl(items = definition)
+  }
+
+  req <- httr2::req_body_form(
+    req,
+    updateDefinition = jsonify::to_json(
+      update_definition,
+      unbox = TRUE
+    ),
+    async = async,
+    f = "json"
   )
 
   resp <- httr2::req_perform(req)
+  body <- httr2::resp_body_json(resp)
 
-  resp
-}
+  # Handle error messages
+  if (rlang::has_name(body, "error")) {
+    message <- body[["error"]][["message"]]
+    details <- unlist(body[["error"]][["details"]])
+    if (message != details) {
+      message <- c(message, details)
+    }
 
-#' @rdname update_definition
-#' @export
-.update_definition_name <- function(...) {
-  rlang::list2(...)
-}
-
-#' @rdname update_definition
-#' @export
-.update_layer_properties <- function(
-    description = NULL,
-    license_text = NULL,
-    xss_trusted_fields = NULL,
-    display_field = NULL,
-    min_scale = NULL,
-    max_scale = NULL,
-    max_records = NULL,
-    default_visibility = NULL,
-    ...) {
-  check_string(description, allow_null = TRUE)
-  check_string(license_text, allow_null = TRUE)
-
-  if (is.character(xss_trusted_fields) && length(xss_trusted_fields) > 1) {
-    xss_trusted_fields <- paste0(xss_trusted_fields, collapse = ", ")
+    cli::cli_abort(message)
   }
 
-  check_string(xss_trusted_fields, allow_null = TRUE)
+  # Display update
+  cli::cli_bullets(c("v" = "Updated definition values:"))
+  cli::cli_dl(items = update_definition)
+  cli::cli_end(dl_theme)
 
-  # FIXME: Avoid using internal arcgisutils functions
-  arcgisutils:::check_number_whole(min_scale, allow_null = TRUE)
-  arcgisutils:::check_number_whole(max_scale, allow_null = TRUE)
-  arcgisutils:::check_number_whole(max_records, allow_null = TRUE)
-  arcgisutils:::check_logical(default_visibility, allow_null = TRUE)
-
-  compact(
-    list(
-      xssTrustedFields = xss_trusted_fields,
-      displayField = display_field,
-      description = description,
-      copywriteText = license_text,
-      minScale = min_scale,
-      maxScale = max_scale,
-      maxRecordCount = max_records,
-      # standardMaxRecordCount = max_records,
-      # tileMaxRecordCount = max_records,
-      # maxRecordCountFactor = 1,
-      # types = "",
-      # templates = "",
-      defaultVisibility = tolower(default_visibility)
-      # typeIdField = ""
-    )
-  )
-}
-
-.update_service_properties <- function(...) {
-  # TODO: Add service property definition
-
-  # {
-  #   "name": "SanFrancisco2",
-  #   "hasStaticData": false,
-  #   "allowGeometryUpdates": "true",
-  #   "maxRecordCount": 2000,
-  #   "serviceDescription": "SanFrancisco2",
-  #   "description": "",
-  #   "copyrightText": "",
-  #   "units": "",
-  #   "xssPreventionInfo": {
-  #     "xssPreventionEnabled": true,
-  #     "xssPreventionRule": "InputOnly",
-  #     "xssInputRule": "rejectInvalid"
-  #   },
-  #   "initialExtent": {
-  #     "xmin": -122.514435102,
-  #     "ymin": 5.6843418860808E-14,
-  #     "xmax": 138.625776397,
-  #     "ymax": 67.1577965990001
-  #   }
-  # }
-  #
+  # TODO: Explore if x should be refreshed w/ updated definition via a call to
+  # arc_open
+  invisible(x)
 }
