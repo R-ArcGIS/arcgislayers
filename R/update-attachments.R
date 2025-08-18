455f5d9e7b30477c96f3fd6439f9de84d86616c5
Josiah Parry <josiah.parry@gmail.com>
2025-08-18 13:14:38 -0700

    update attachments

--- R/update-attachments.R
+++ R/update-attachments.R
@@ -1,5 +1,15 @@
-# https://developers.arcgis.com/rest/services-reference/enterprise/update-attachment/#request-parameters
-
+#' Update Feature Service Attachments
+#'
+#' Feature Services can contain attachments that are associated with a single feature ID. This function enables you to update the attachments of multiple features at once by generating multiple requests and performing them in parallel.
+#'
+#' To rename or otherwise an attachment in a Feature Service, you must first download that attachment, modify the file on disk, and then upload it again. This is a limitation of ArcGIS Online and Enterprise. If you'd like to see this changed, please submit a community idea at [community.esri.com](https://community.esri.com/t5/arcgis-online/ct-p/arcgis-online).
+#'
+#' @inheritParams arc_open
+#' @param feature_id a vector of object IDs that corresponds to the feature of the corresponding `attachment_id`.
+#' @param attachment_id the ID of the attachment—this corresponds to the `id` column returned from `query_layer_attachments()`
+#' @returns a `data.frame` with 2 columns returning the status of the update.
+#' @references See [API documentation](https://developers.arcgis.com/rest/services-reference/enterprise/update-attachment/#request-parameters) for more.
+#' @export
 update_attachments <- function(
   x,
   feature_id,
@@ -22,6 +32,17 @@ update_attachments <- function(
   if (anyNA(attachment_id)) {
     cli::cli_abort("{.arg attachment_id} must not contain missing values")
   }
+
+  if (!rlang::is_character(feature_id) && !rlang::is_integer(feature_id)) {
+    cli::cli_abort("{.arg feature_id} must be a character or integer vector")
+  }
+
+  if (anyNA(feature_id)) {
+    cli::cli_abort("{.arg feature_id} must not contain missing values")
+  }
+
+  # broadcast in the case of a scalar
+  feature_id <- broadcast(feature_id, attachment_id)
 
   if (!rlang::is_character(path)) {
     cli::cli_abort("{.arg path} must be a character vector")
@@ -102,4 +123,36 @@ update_attachments <- function(
   }
 
   res
+}
+
+
+#' Broadcast x to the same length as y
+#'
+#' Broadcasts the argument `x` to the same length as `y`.
+#'
+#' @param x a scalar atomic or an atomic of the same length as `y`
+#' @param y an atomic vector
+#' @noRd
+#' @keywords internal
+broadcast <- function(x, y) {
+  if (!rlang::is_bare_atomic(x) || !rlang::is_bare_atomic(y)) {
+    rlang::abort("`x` and `y` must be atomic vectors")
+  }
+
+  if (typeof(x) != typeof(y)) {
+    rlang::abort("`x` and `y` must be the same type")
+  }
+
+  len_y <- length(y)
+  len_x <- length(x)
+
+  if (len_x == 1L) {
+    return(rep(x, len_y))
+  }
+
+  if (len_x != len_y) {
+    rlang::abort("`x` must be a scalar or the same length as `y`")
+  }
+
+  x
 }
